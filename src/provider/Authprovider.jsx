816bb595cd47502924087e1466273864afae382f
Habib <habibleo861@gmail.com>
2025-01-30 14:34:34 +0600

    AUthprovider

---
+++ src/provider/Authprovider.jsx
@@ -0,0 +1,54 @@
+import { createContext, useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+} from "firebase/auth";
+import app from "./../firebase/firebase.config";
+
+export const AuthContext = createContext();
+const auth = getAuth(app);
+const provider= new GoogleAuthProvider();
+const Authprovider = ({ children }) => {
+    
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth,email, password);
+  };
+  const signinwithemail=(email,password)=>{
+    setLoading(true)
+    return signInWithEmailAndPassword(auth,email,password)
+  }
+  const signinwithgoogle=()=>{
+    setLoading(true)
+    signInWithPopup(auth,provider);
+  }
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentuser) => {
+      setUser(currentuser);
+      setLoading(false);
+      return () => {
+        return unsubscribe();
+      };
+    });
+  }, []);
+  const authinfo = {
+    user,
+    loading,
+    createUser,
+    signinwithemail,
+    signinwithgoogle,
+  };
+
+  return (
+    <AuthContext.Provider value={authinfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default Authprovider;
