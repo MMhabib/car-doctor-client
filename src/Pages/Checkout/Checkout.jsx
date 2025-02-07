6d8e8860db589d74cf47519cae3e774629678c01
Habib <habibleo861@gmail.com>
2025-02-07 19:36:06 +0600

    checkout

--- src/Pages/Checkout/Checkout.jsx
+++ src/Pages/Checkout/Checkout.jsx
@@ -1,47 +1,60 @@
 import { useLoaderData } from "react-router-dom";
 
 const Checkout = () => {
+
+const handlebooking=event=>{
+  event.preventDefault()
+
+  const form = event.target;
+  const name= form.name.value;
+  const date=form.date.value ;
+  const email =form.email.value;
+const due=form.due.value;
+
+console.log(name,due,email,date);
+  
+}
+
   const service = useLoaderData();
-  const { title, facility } = service;
+  const { title, facility ,price} = service;
   return (
     <div>
-      <h1>book now</h1>
-      <h1>{title}</h1>
-      <p>{facility[0].name}</p>
-      <p>{facility[1].name}</p>
+      <h1 >book now</h1>
+      <h1 className="text-3xl text-center">{title}</h1>
+      
       
      
-      <form className="card-body">
+      <form onSubmit={handlebooking} className="card-body">
        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
        <div className="form-control ">
           <label className="label">
-            <span className="label-text">Email</span>
+            <span className="label-text">Name</span>
           </label>
-          <input type="email" placeholder="email" className="input w-full input-bordered" required />
+          <input type="text" placeholder="Name" name="name" className="input w-full input-bordered" required />
         </div>
         <div className="form-control">
           <label className="label">
-            <span className="label-text">Password</span>
+            <span className="label-text">Appointment Date</span>
           </label>
-          <input type="password" placeholder="password" className="input w-full input-bordered" required />
+          <input type="date" placeholder="Date"  name="date" className="input w-full input-bordered" required />
           
         </div>
        <div className="form-control">
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="email" placeholder="email" className="input w-full input-bordered" required />
+          <input type="email" placeholder="email" name="email" className="input w-full input-bordered" required />
         </div>
         <div className="form-control">
           <label className="label">
-            <span className="label-text">Password</span>
+            <span className="label-text">Due Amount</span>
           </label>
-          <input type="password" placeholder="password" className="input  w-full input-bordered" required />
+          <input type="text" name="due" defaultValue={'$'+price} className="input  w-full input-bordered" required />
           
         </div>
        </div>
         <div className="form-control mt-6 ">
-          <button className="btn btn-primary w-full">Login</button>
+          <input className="btn btn-primary w-full" type="submit" value="Book Now" />
         </div>
       </form>
     </div>
